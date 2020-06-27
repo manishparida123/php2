69a38224333a4d4c43849b2050422ace27fc4175
Manish Parida <manishparida123@gmail.com>
2020-06-28 01:07:56 +0530

    changed db connection

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "detsdb");
+$con=mysqli_connect("mysqldatabase.cyinc84ll8ue.ap-south-1.rds.amazonaws.com", "admin", "admin123", "detsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
